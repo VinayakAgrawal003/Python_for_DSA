7178c6cc6239c098e81d4d9ae89780ded82abe57
Vinayak Agrawal <vinayakagrawal003@gmail.com>
2024-10-14 10:53:44 +0530

    topics.py

---
+++ topics.py
@@ -0,0 +1,11 @@
+topics :
+1. Python Basics ( Background part,Why we need programing, why are you dooing Python, Introduction)
+2. Variables 
+3. Input-Output
+4. Flow Control
+5. Operators
+6. Loop 
+7. Functions 
+8. String
+9. Loops
+10. Objest Oriented Programing 
