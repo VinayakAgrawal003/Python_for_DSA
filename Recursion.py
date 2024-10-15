38aaac88c0228edf71d641cce53efe6d2b33cdc8
Vinayak Agrawal <vinayakagrawal003@gmail.com>
2024-10-15 21:14:30 +0530

    Update Recursion.py

--- Recursion.py
+++ Recursion.py
@@ -1,1 +1,18 @@
+Q1. What is recursion?
+Ans. Recursion in the function calling itself again and again.
 
+Q2. Why do we use it?
+Ans. We can write our code without recursion, but we must use it for big files. Especially if we talk of projects it is good to use it.
+     We use it to make our code more optimal and readable and reduce time complexity.
+  
+Q3 . How to use it?
+Ans. Write a function, write logic inside it, and call that function again.
+
+Questions for practice : 
+NOTE: ALL THE BELOW QUESTIONS YOU HAVE TO DO WITH THR RECURSION.
+
+1. Print your name n times.
+2. Print linearly from 1 to n.
+3. Print reversed from 1 to n.
+4. Print the sum of 1st n numbers. 
+     
